3609a27113b5910f5b7ce889865a52254209ca40
Maritana Alves <maritana.cz@gmail.com>
2022-04-12 15:37:35 -0300

    Exercicio 06

---
+++ 06_Exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodofuncionario;
+            double horastrabalhadas, valorhora, salario;
+
+            numerodofuncionario = int.Parse(Console.ReadLine());
+            horastrabalhadas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valorhora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = horastrabalhadas * valorhora;
+
+            Console.WriteLine("O número do funcionário é: " + numerodofuncionario);
+            Console.WriteLine("O salário do funcionário é: R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
