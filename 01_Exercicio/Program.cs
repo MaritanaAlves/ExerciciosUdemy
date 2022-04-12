7c27337219b46a4f98b56ca8ad4efad09b11679a
Maritana Alves <maritana.cz@gmail.com>
2022-04-12 13:49:04 -0300

    Exercicios Udemy

---
+++ 01_Exercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5298;
+            char genero = 'F';
+
+            double preco1 = 2100.00;
+            double preco2 = 350.00;
+            double medida = 53.234567;
+
+            Console.WriteLine($"Produtos: {produto1}, cujo o preço é R$ {preco1}");
+            Console.WriteLine($"{produto2}, cujo o preço é R$ {preco2}");
+            Console.WriteLine();
+            Console.WriteLine($"Registro: {idade} anos, código: {codigo} genero: {genero}");
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimas: {medida.ToString("F8")}");
+            Console.WriteLine($"arredondando (três casas decimais): {medida.ToString("F3")}");
+            Console.WriteLine($"Separador decimal invariante cuture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace _02_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = (Console.ReadLine());
+            int numerodequartos = int.Parse(Console.ReadLine());
+            double preco = double.Parse(Console.ReadLine());
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimonome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nome);
+            Console.WriteLine(numerodequartos);
+            Console.WriteLine(preco);
+
+            Console.WriteLine(ultimonome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2"), CultureInfo.InvariantCulture);
+
+            Console.ReadKey();
+        }
+    }
+}
