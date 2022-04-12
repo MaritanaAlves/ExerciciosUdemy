878cc514cdd6f8ebf7f5fea879086ed881e5d707
Maritana Alves <maritana.cz@gmail.com>
2022-04-12 15:03:37 -0300

    Exercício 04

---
+++ 04_Exercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _04_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area;
+            double pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = pi * (raio * raio);
+
+            Console.WriteLine("A área é igual a: " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
