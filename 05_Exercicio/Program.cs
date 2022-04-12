8ae525b3452fb43f9ac04011af2f5238ca34c36a
Maritana Alves <maritana.cz@gmail.com>
2022-04-12 15:20:20 -0300

    Exercicio 05

---
+++ 05_Exercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, diferenca;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            diferenca = (a * b) - (c * d);
+
+            Console.WriteLine("A diferença do produto de A e B pelo produto de C e D é: " + diferenca);
+            Console.ReadKey();
+        }
+    }
+}
