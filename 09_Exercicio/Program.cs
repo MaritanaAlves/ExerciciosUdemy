20823b1beca3850b308935a5e80a87c899517d3c
Maritana Alves <maritana.cz@gmail.com>
2022-04-13 14:20:08 -0300

    Exercicio 9

---
+++ 09_Exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+            else
+            {
+                Console.WriteLine("Número ímpar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
