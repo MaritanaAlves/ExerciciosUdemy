d2e68e9c68d43a7318405a23b4866d6fa36ccd28
Maritana Alves <maritana.cz@gmail.com>
2022-04-13 14:11:16 -0300

    Exercicio 8

---
+++ 08_Exercicio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x > 0) {
+                Console.WriteLine("Número positivo");
+            }
+            else
+            {
+                Console.WriteLine("Número negativo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
