7e1c879c7b71f377a629670964a11251da6edd20
Maritana Alves <maritana.cz@gmail.com>
2022-04-15 18:10:01 -0300

    Adicionado exercicio 11

---
+++ 11_Exercicio/Funcionarios.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_Exercicio
+{
+    internal class Funcionarios
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ 11_Exercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace _11_Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios funcionario1 = new Funcionarios();
+            Funcionarios funcionario2 = new Funcionarios();
+
+            Console.Write("Digite o nome do primeiro funcionário: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Digite o salário do primeiro funcionário: ");
+            funcionario1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o nome do segundo funcionário: ");
+            funcionario2.nome = Console.ReadLine();
+            Console.Write("Digite o salário do segundo funcionário: ");
+            funcionario2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (funcionario1.salario + funcionario2.salario) / 2;
+
+            Console.WriteLine($"O salário médio dos funcionários é: R$ {media}");
+        }
+    }
+}
