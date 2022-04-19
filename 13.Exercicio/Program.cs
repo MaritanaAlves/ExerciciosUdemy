742d3f7d9e38c3ea43cd59e7644c98a493a3359f
Maritana Alves <maritana.cz@gmail.com>
2022-04-19 13:55:02 -0300

    Terminado Exercicio 13

--- 13.Exercicio/Program.cs
+++ 13.Exercicio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _13.Exercicio
 {
@@ -6,7 +7,19 @@ namespace _13.Exercicio
     {
         static void Main(string[] args)
         {
-            
-        }
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Ente com a largura e altura do retângulo, respectivamente: ");
+            Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("Perímetro = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("Diagonal = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        } 
     }
 }
--- 13.Exercicio/Retangulo.cs
+++ 13.Exercicio/Retangulo.cs
@@ -6,7 +6,20 @@ namespace _13.Exercicio
 {
     class Retangulo
     {
-        public double largura;
-        public double altua;
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt (Largura * Largura + Altura * Altura);
+        }
     }
 }
