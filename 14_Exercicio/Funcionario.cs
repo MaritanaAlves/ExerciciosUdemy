8ff76298df36481b45215696dd77f7297f10ecd0
Maritana Alves <maritana.cz@gmail.com>
2022-04-20 14:05:11 -0300

    Exercicio incompleto

---
+++ 14_Exercicio/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14_Exercicio
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+    }
+}
---
+++ 14_Exercicio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Digite os dados do Funcionário: ");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse (Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse (Console.ReadLine());
+
+            Console.Write("Funcionário: " + f.Nome + )
+
+            Console.ReadKey();
+        }
+    }
+}
