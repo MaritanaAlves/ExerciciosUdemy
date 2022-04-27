b84b744575b3267b54ad622fc556ebd45568d936
Maritana Alves <maritana.cz@gmail.com>
2022-04-27 15:18:46 -0300

    adicionado exercicio

---
+++ 19_Exercicio/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace _19_Exercicio
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+    }
+}
---
+++ 19_Exercicio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace _19_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Preco;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("O preço médio é: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
