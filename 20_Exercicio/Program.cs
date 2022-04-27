05a8c77714e6493cdc3bc15405c5181b40a86507
Maritana Alves <maritana.cz@gmail.com>
2022-04-27 16:13:17 -0300

    adicionado exercicio

---
+++ 20_Exercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _20_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registro[] vect = new Registro[10];
+
+            Console.Write("Quantos quartos vão ser alugados? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.WriteLine();
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("e-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Registro(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }    
+    }
+}
---
+++ 20_Exercicio/Registro.cs
@@ -0,0 +1,21 @@
+﻿namespace _20_Exercicio
+{
+    class Registro
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        
+        public Registro (string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
