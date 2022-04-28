31724fbcdce48b393d5ab612d5cd000ef3e5080a
Maritana Alves <maritana.cz@gmail.com>
2022-04-28 15:39:51 -0300

    22 Incompleto, adicionado exercicio 23

---
+++ 22_Exercicio/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_Exercicio
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee (int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increasetSalary(double percentage)
+        {
+            return Salary += x / 100;
+        }
+
+    }
+}
---
+++ 22_Exercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _22_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered: ");
+            int nEmployees = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
