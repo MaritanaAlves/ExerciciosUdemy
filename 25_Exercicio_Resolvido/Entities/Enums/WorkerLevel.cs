bc688d792e4f2b4a78d808257e10e14d46d5e586
Maritana Alves <maritana.cz@gmail.com>
2022-05-02 18:23:04 -0300

    Exercicio incompleto

---
+++ 25_Exercicio_Resolvido/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace _25_Exercicio_Resolvido.Entities.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel =  1,
+        Senior = 2
+    }
+}
---
+++ 25_Exercicio_Resolvido/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _25_Exercicio_Resolvido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+
+
+        }
+    }
+}
