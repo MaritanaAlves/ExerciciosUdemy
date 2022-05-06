52feda153d49abc330f07d623794b0e2fac6a42e
MaritanaAlves <maritana.cz@gmail.com>
2022-05-06 15:15:30 -0300

    Adicionado exercicio

---
+++ 26_Exercicio_EnumeracoesEComposicoes/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace _26_Exercicio_EnumeracoesEComposicoes.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
