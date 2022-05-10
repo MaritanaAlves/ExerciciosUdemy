7b9abf7965b3c6b94265827e3f01904d47e89cc1
MaritanaAlves <maritana.cz@gmail.com>
2022-05-10 18:00:28 -0300

    exercicio incompleto

---
+++ 32_Exercicio_CriandoExcecoesPersonalizadas1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using _32_Exercicio_CriandoExcecoesPersonalizadas1.Entities;
+
+namespace _32_Exercicio_CriandoExcecoesPersonalizadas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+            }
+
+            
+
+        }
+    }
+}
