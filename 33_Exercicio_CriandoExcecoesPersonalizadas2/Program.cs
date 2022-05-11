69da965d2f96dd37671a8f3f1d2ff129a0f0f2cc
MaritanaAlves <maritana.cz@gmail.com>
2022-05-11 14:41:39 -0300

    Exercicio adicionado

---
+++ 33_Exercicio_CriandoExcecoesPersonalizadas2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using _33_Exercicio_CriandoExcecoesPersonalizadas2.Entities;
+
+namespace _33_Exercicio_CriandoExcecoesPersonalizadas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                if (error != null)
+                {
+                    Console.WriteLine("Error in reservation: " + error);
+                }
+                
+                else
+                {
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
