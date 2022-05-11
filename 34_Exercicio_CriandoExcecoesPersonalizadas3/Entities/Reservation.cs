c57c08542de1e078ccc364387d8de01dc5b86c7f
MaritanaAlves <maritana.cz@gmail.com>
2022-05-11 15:49:07 -0300

    Adicionado Exercicio

---
+++ 34_Exercicio_CriandoExcecoesPersonalizadas3/Entities/Reservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using _34_Exercicio_CriandoExcecoesPersonalizadas3.Entities.Exceptions;
+
+namespace _34_Exercicio_CriandoExcecoesPersonalizadas3.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new Exception("Check-out dare must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new Exception ("Raservation dates for update must be future dates");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new Exception ("Check-out dare must be after check-in date");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room"
+                + RoomNumber
+                + ", Check-In: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Check-Out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + "night";
+        }
+    }
+}
---
+++ 34_Exercicio_CriandoExcecoesPersonalizadas3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using _34_Exercicio_CriandoExcecoesPersonalizadas3.Entities;
+
+namespace _34_Exercicio_CriandoExcecoesPersonalizadas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation: ");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            reservation.UpdateDates(checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+        }
+    }
+}
