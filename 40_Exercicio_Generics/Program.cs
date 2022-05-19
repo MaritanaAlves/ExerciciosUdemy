76303e6a9d60036758b047789a0ee304277c7e3f
MaritanaAlves <maritana.cz@gmail.com>
2022-05-19 17:21:24 -0300

    Adicionado exercicios

---
+++ 40_Exercicio_Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _40_Exercicio_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int values = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < values; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
---
+++ 41_Exercicio_RestricoesDeGenerics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using _41_Exercicio_RestricoesDeGenerics.Services;
+using _41_Exercicio_RestricoesDeGenerics.Entities;
+using System.Globalization;
+
+namespace _41_Exercicio_RestricoesDeGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.Write(max);
+        }
+    }
+}
