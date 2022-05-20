36ad060abdc572efaf0f8d1a52f5e3306fd8aa90
MaritanaAlves <maritana.cz@gmail.com>
2022-05-20 17:57:19 -0300

    exercicio imcompleto

--- 43_Exercicio_HashSetESortedSet/Program.cs
+++ 43_Exercicio_HashSetESortedSet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _43_Exercicio_HashSetESortedSet
 {
@@ -6,7 +7,32 @@ namespace _43_Exercicio_HashSetESortedSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            //union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            printCollection(c);
+
+            //intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.UnionWith(b);
+            printCollection(d);
+
+            //difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.UnionWith(b);
+            printCollection(e);
+        }
+
+        static void printCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ 44_Exercicio_ColecoesHash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _44_Exercicio_ColecoesHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
