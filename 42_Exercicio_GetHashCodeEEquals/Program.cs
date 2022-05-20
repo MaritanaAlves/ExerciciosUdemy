3a55bbb97ef828bf2b9705f1af5b6ede60e3629a
MaritanaAlves <maritana.cz@gmail.com>
2022-05-20 15:12:49 -0300

    Exercicio Adicionado

---
+++ 42_Exercicio_GetHashCodeEEquals/Program.cs
@@ -0,0 +1,16 @@
+﻿using _41_Exercicio_GetHashCodeEEquals.Entities;
+using System;
+
+namespace _41_Exercicio_GetHashCodeEEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "João", Email = "joao@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
