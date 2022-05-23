8140e352949e73750629f89390a80db2c1e185b4
MaritanaAlves <maritana.cz@gmail.com>
2022-05-23 14:35:03 -0300

    Exercicio concluído

--- 43_Exercicio_HashSetESortedSet/Program.cs
+++ 43_Exercicio_HashSetESortedSet/Program.cs
@@ -10,17 +10,17 @@ namespace _43_Exercicio_HashSetESortedSet
             SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
             SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
 
-            //union
+            //Union
             SortedSet<int> c = new SortedSet<int>(a);
             c.UnionWith(b);
             printCollection(c);
 
-            //intersection
+            //Intersection
             SortedSet<int> d = new SortedSet<int>(a);
             d.UnionWith(b);
             printCollection(d);
 
-            //difference
+            //Difference
             SortedSet<int> e = new SortedSet<int>(a);
             e.UnionWith(b);
             printCollection(e);
