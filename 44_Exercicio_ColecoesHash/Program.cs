ac8ab794605180d575452eb1226ca4e4040f20e4
MaritanaAlves <maritana.cz@gmail.com>
2022-05-23 15:37:48 -0300

    Exercicio concluido

--- 44_Exercicio_ColecoesHash/Program.cs
+++ 44_Exercicio_ColecoesHash/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using _44_Exercicio_ColecoesHash.Entities;
 
 namespace _44_Exercicio_ColecoesHash
 {
@@ -6,7 +8,16 @@ namespace _44_Exercicio_ColecoesHash
     {
         static void Main(string[] args)
         {
-            
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1500.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
         }
     }
 }
